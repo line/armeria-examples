58cbb265dba96a0040050b61a7beccf10b80468c
shalk(xiao kun) <xshalk@163.com>
2024-05-07 21:21:51 +0800

    update pom.xml and add scrip in update-example

--- update-examples.sh
+++ update-examples.sh
@@ -50,6 +50,7 @@ if [[ ! -d "$SRC_DIR/.git" ]]; then
   exit 1
 fi
 cd "$(dirname "$0")"
+CUR_DIR=`pwd`
 
 echo 'Copying README.md ..'
 cp -f "$SRC_DIR/examples/README.md" .
@@ -303,5 +304,20 @@ for E in $(find_examples); do
   ./gradlew -q clean check
   popd >/dev/null
 done
+
+echo 'Generate maven pom.xml for examples ...'
+# clone shalk/armeria-tom4j to `.mvn/armeria-tom4` or pull if exists
+MVN_TOOL_DIR=".mvn/armeria-tom4j"
+if [[ -d $MVN_TOOL_DIR ]]
+then
+  pushd $MVN_TOOL_DIR && git pull && popd
+else
+  git clone https://github.com/shalk/armeria-tom4j $MVN_TOOL_DIR
+fi
+
+pushd $MVN_TOOL_DIR || exit 1
+echo  mvn compile exec:java -Dexec.args="$VERSION $SRC_DIR $CUR_DIR"
+popd $MVN_TOOL_DIR
+
 
 echo "Successfully updated the examples for Armeria $VERSION"
