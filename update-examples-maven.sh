55a1e4d6c1bcede793840737f1833f724f955d30
shalk(xiao kun) <xshalk@163.com>
2024-05-08 11:58:39 +0800

    clean kotlin and scala

---
+++ update-examples-maven.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+
+function generate_maven_pom_xml() {
+    local VERSION=$1
+    local SRC_DIR=$2
+    local CUR_DIR=$3
+
+    echo 'Generate maven pom.xml for examples ...'
+    # clone shalk/armeria-tom4j to `.mvn/armeria-tom4` or pull if exists
+    local MVN_TOOL_DIR=".mvn/armeria-tom4j"
+    if [[ -d $MVN_TOOL_DIR ]]
+    then
+      pushd $MVN_TOOL_DIR && git pull && popd
+    else
+      git clone https://github.com/shalk/armeria-tom4j $MVN_TOOL_DIR
+    fi
+
+    pushd $MVN_TOOL_DIR || exit 1
+    mvn clean compile exec:java -Dexec.args="$VERSION $SRC_DIR $CUR_DIR"
+    popd $MVN_TOOL_DIR
+}
+
+CUR_DIR=$(pwd)
+generate_maven_pom_xml "$VERSION" "$SRC_DIR" "$CUR_DIR"
--- update-examples.sh
+++ update-examples.sh
@@ -50,7 +50,6 @@ if [[ ! -d "$SRC_DIR/.git" ]]; then
   exit 1
 fi
 cd "$(dirname "$0")"
-CUR_DIR=`pwd`
 
 echo 'Copying README.md ..'
 cp -f "$SRC_DIR/examples/README.md" .
@@ -305,19 +304,6 @@ for E in $(find_examples); do
   popd >/dev/null
 done
 
-echo 'Generate maven pom.xml for examples ...'
-# clone shalk/armeria-tom4j to `.mvn/armeria-tom4` or pull if exists
-MVN_TOOL_DIR=".mvn/armeria-tom4j"
-if [[ -d $MVN_TOOL_DIR ]]
-then
-  pushd $MVN_TOOL_DIR && git pull && popd
-else
-  git clone https://github.com/shalk/armeria-tom4j $MVN_TOOL_DIR
-fi
-
-pushd $MVN_TOOL_DIR || exit 1
-mvn compile exec:java -Dexec.args="$VERSION $SRC_DIR $CUR_DIR"
-popd $MVN_TOOL_DIR
-
+. update-examples-maven.sh
 
 echo "Successfully updated the examples for Armeria $VERSION"
